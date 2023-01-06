5c36e1924f577a059c4f84690547f7f234b46355
j4son3099 <121943674+j4son3099@users.noreply.github.com>
2023-01-06 00:06:51 +0000

    fully working update

--- mySketch.js
+++ mySketch.js
@@ -1,6 +1,6 @@
 //CC Attribution “Spinning cylinders” by Lisa Sekaida http://openprocessing.org/sketch/1742535
 //License CreativeCommons Attribution NonCommercial ShareAlike https://creativecommons.org/licenses/by-nc-sa/3.0
-
+//noprotect
 let fInput;
 var img = null
 var img2 = null
@@ -10,10 +10,12 @@ let mainCanvas
 let graphicCanvas
 let filename 
 let loaded = false
+let pixelsLoaded = false
 let cSize = 20
 let WIDTH 
 let HEIGHT 
 let positionArray = []
+let autopaintThreshold = 50
 
 
 function preload(){
@@ -48,7 +50,7 @@ function draw() {
 	
 
 	
-if (img!=null&&img2!=null&&!loaded) {
+if (img!=null&&img2!=null&&pixelsLoaded&&!loaded) {
 	console.log("loaded")
 	loaded = true
 	//graphicCanvas.background(100)
@@ -59,34 +61,35 @@ if (img!=null&&img2!=null&&!loaded) {
 	fInput.hide()
 	
 	
-	img2.loadPixels();
-	let index = 0
-	for (let i = 0; i < img.width; i++) {
-		for (let j = 0; j < img.height; j++) {
-			if (img2.pixels[index]+img2.pixels[index+1]+img2.pixels[index+2]<50) positionArray.push([i,j]) 
-			index=index+4
-		}
-	}
 	
 	
-	for(let i=0;i<positionArray.length;i++){
-		 	push()
-			translate(positionArray[i][0],positionArray[i][1],0)
-			rotateZ(PI/64);
-			rotateX(frameCount * 0.001);
-			fill(i*c[m][0], i* c[m][1], i*c[m][2], 63);
-			cylinder(cSize);
-			pop()
-	}
 	
 
 clear()
 	push()
-	translate(0,0,-500)
+	translate(0,0,-10)
 
 	texture(graphicCanvas)
 	//rotateX(Math.PI/2)
 	plane(WIDTH,HEIGHT)
+	pop()
+	
+	console.log("ArrayLength:" + positionArray.length)
+	push()
+	
+	translate(-WIDTH/2>>0,-HEIGHT/2>>0,0)
+	
+	for(let i=0;i<positionArray.length;i++){
+			
+		 	push()
+			//console.log("position Array: " + positionArray[i])
+			translate(positionArray[i][0],positionArray[i][1],-10)
+			rotateZ(Math.PI/64);
+			rotateX(i * 0.001);
+			fill(i*c[m][0]%255, i*c[m][1]%255, i*c[m][2]%255, 63);
+			cylinder(cSize);
+			pop()
+	}
 	pop()
 }
 
@@ -107,9 +110,26 @@ if (m<3){
 function handleFile(file) {
   print(file);
   if (file.type === 'image') {
-    img = createImg(file.data, "");
+		img = createImg(file.data, "");
 		img.hide()
-		img2 = loadImage(file.data)
+		img2 = createImage(WIDTH, HEIGHT)
+		if (!pixelsLoaded) loadImage(file.data, img2 => { 
+																	img2.resize(WIDTH,HEIGHT)
+																	img2.loadPixels();
+																 let index = 0
+
+																	for (let i = 0; i < img2.width; i++) {
+																		for (let j = 0; j < img2.height; j++) {
+																			index = (i +j*img2.width)*4;
+																			//console.log("pixels[index]: " +img2.pixels[index]) 
+																			if (img2.pixels[index]<autopaintThreshold&&img2.pixels[index+1]<autopaintThreshold&&img2.pixels[index+2]<autopaintThreshold) positionArray.push([i,j])
+																			
+																		}
+																	}
+																	pixelsLoaded = true
+																  console.log("pixels loaded") 
+																 })
+		
   } else {
     img = null;
 		img2 = null
@@ -119,7 +139,7 @@ function handleFile(file) {
 //rotate brushes on mouse click
 function mouseClicked() {
 	m++
-	m=m%3
+	m=m%4
 }
 
 function keyPressed(){
@@ -127,4 +147,5 @@ function keyPressed(){
 	if (key=="s") mainCanvas.get().save(filename, "png");
 	if (key=="z") cSize = Math.max(8,cSize-5*random()>>0)
 	if (key=="x") cSize = Math.min(90,cSize+5*random()>>0)
+	return 0
 }
